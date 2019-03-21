539f01df73ab584d3ccf3c528d4adb6fcb8a117c
Sofiia <sofiiabondarenko91@gmail.com>
2019-03-21 18:42:29 +0200

    commit from console

--- src/com/vogella/eclipse/ide/first/FishMaster.java
+++ src/com/vogella/eclipse/ide/first/FishMaster.java
@@ -11,8 +11,8 @@ public class FishMaster {
 
     public static void main(String[] args) {
         Fish myFish = new Fish();
-//        myFish.dive(2);
-//        myFish.dive(3);
+        myFish.dive(2);
+        myFish.dive(3);
         myFish.say("Hello!");
     }
 }
