02359248d0af14007c9cef30af5cf249dcf5f444
Sofiia <sofiiabondarenko91@gmail.com>
2019-03-21 17:25:25 +0200

    homeWork

--- src/com/atm/ATM.java
+++ src/com/atm/ATM.java
@@ -5,10 +5,16 @@ public class ATM {
     public static void main(String[] args) {
         CreditCard myCreditCard = new CreditCard();
 
+        myCreditCard.getSomeMoney(0);
+
         // У вас теперь на счету: 100
-        myCreditCard.putSomeMoneyUp(100);
+        myCreditCard.putSomeMoneyUp(0);
 
         // Остаток по счету: 70
-        myCreditCard.putSomeMoneyUp(30);
+        myCreditCard.putSomeMoneyDown(0); //не нашла как сделать вычисления с этим же методом putSomeMoneyUp
+
+
     }
 }
+
+
--- src/com/atm/CreditCard.java
+++ src/com/atm/CreditCard.java
@@ -1,19 +1,34 @@
 package com.atm;
 
 public class CreditCard {
+
+    int amount = 0;
+
+
     // Конструктор который принимает сумму денег
-    public CreditCard () {}
+    public CreditCard() {
+    }
 
     // Снять с карты
-    public int getSomeMoney (int amount) {
+    public int getSomeMoney(int amount) {
         // 1) вывести в консоль остаток по счету: "Остаток по счету: {некая_сумма}"
         // 2) возвращаем запрашевоемую сумму
-        return 0;
+        System.out.println("Остаток по счету:" + amount);
+        return this.amount;
     }
 
     // Пополнить карту
-    public void putSomeMoneyUp (int amount) {
+    public void putSomeMoneyUp(int amount) {
         // 1) добавить сумму на счет
         // 2) вывести в консоль фразу: "У вас теперь на счету: {некая_сумма}"
+        amount = amount + 100;
+        System.out.println("У вас теперь на счету:" + amount);
     }
+
+    public void putSomeMoneyDown(int amount) {
+        amount = amount + 70;
+        System.out.println("У вас теперь на счету:" + amount);
+
+    }
+
 }
